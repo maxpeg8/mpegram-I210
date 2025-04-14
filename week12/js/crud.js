2b326779c8413abfe385bd9bb164fbb8a7422985
Max Pegram <maxpeg850@gmail.com>
2025-04-14 14:45:12 -0400

    week 12

--- week12/js/crud.js
+++ week12/js/crud.js
@@ -1,7 +1,5 @@
 import { app } from "./app.js";
 
-console.log(app);
-
 import {
   getFirestore,
   getDocs,
@@ -9,15 +7,7 @@ import {
   updateDoc,
   deleteDoc,
   doc,
-} from "https://www.gstatic.com/firebasejs/11.6.0/firebase-app.js";
-
-console.log(getFirestore);
-
-console.log(getDocs);
-console.log(collection);
-console.log(updateDoc);
-console.log(deleteDoc);
-console.log(doc);
+} from "https://www.gstatic.com/firebasejs/11.6.0/firebase-firestore.js";
 
 const db = getFirestore(app);
 
@@ -26,27 +16,66 @@ const dreamsCollection = collection(db, "hopesdreams");
 const dreamsRef = document.querySelector("#dreams");
 
 async function getDreams() {
-  const dreamsDocs = getDocs(dreamsCollection);
+  const dreamsDocs = await getDocs(dreamsCollection);
+
   dreamsRef.innerHTML = "";
 
   for (let i = 0; i < dreamsDocs.docs.length; i++) {
     const currentDream = dreamsDocs.docs[i];
 
     const data = currentDream.data();
+    const hearts = data.hearts || 0;
 
-    dreamsRef.innerHTML += ` <div class="dream">
-        <h4>${data.text}</h4>
-        <p>Likes: ${data.hearts || 0}</p>
+    dreamsRef.innerHTML += `
+      <div class="dream">
+        <h4>
+        <span class="delete" data-id="${currentDream.id}">&otimes;</span>
+        ${data.text}</h4>
+        <p>Likes: ${hearts}</p>
         <p>
           <button class="edit">Edit</button>
-          <button class="heart">&hearts;</button>
+          <button class="heart" data-id="${currentDream.id}" data-hearts="${hearts}">&hearts;</button>
         </p>
       </div>`;
   }
+
+  const heartsRef = document.querySelectorAll(".heart");
+
+  for (let i = 0; i < heartsRef.length; i++) {
+    heartsRef[i].onclick = addHeart;
+  }
+
+  const otimesRef = document.querySelectorAll(".delete");
+
+  for (let i = 0; i < otimesRef.length; i++) {
+    otimesRef[i].onclick = forgetDream;
+  }
 }
 
-function addHearts(e) {
-  console.log("Add heart", e);
+async function addHeart(e) {
+  console.log("Add heart", e.target.dataset.id);
+
+  const newHeartCount = Number(e.target.dataset.hearts) + 1;
+
+  const dreamToUpdate = doc(dreamsCollection, e.target.dataset.id);
+
+  await updateDoc(dreamToUpdate, { hearts: newHeartCount });
+
+  //   alert("Dream updated");
+  getDreams();
+}
+
+async function forgetDream(e) {
+  console.log("Dream to forget", e.target.dataset.id);
+
+  const userConfirmed = confirm("Are you sure you want to forget this dream?");
+
+  if (userConfirmed) {
+    const dreamToDelete = doc(dreamsCollection, e.target.dataset.id);
+    await deleteDoc(dreamToDelete);
+
+    getDreams();
+  }
 }
 
 getDreams();
